6f63d6b9f95b84c985213af7df858174385f903c
João Faria <joao.faria@astro.up.pt>
2020-11-17 19:48:09 +0100

    more accurate acf normalization

--- scalpels.py
+++ scalpels.py
@@ -2,7 +2,8 @@
 import numpy as np
 
 def ccf2acf(ccf):
-    """ Compute the ACF of each CCF in the time series (eq. 3 in [1])
+    """
+    Compute the ACF of each CCF (eq. 3 in [1])
 
     Args:
         ccf (array, Nobs x Nrv) Cross-correlation function for each observation
@@ -13,20 +14,23 @@ def ccf2acf(ccf):
         [1] Collier Cameron et al. (2020) arxiv:2011.00018
     """
     ccf = np.atleast_2d(ccf)
+
     # number of CCF "pixels" or velocity bins
     Nrv = ccf.shape[1]
+
     # normalize by the mean CCF of each observation
     ccf_norm = ccf / ccf.mean(axis=1, keepdims=True)
 
     # to normalize the ACF to 1
     acf0 = (ccf**2).sum(axis=1)
 
-    acf = np.zeros_like(ccf)
+    # calculate ACF
+    acf = []
     for i in range(Nrv):
         ccf_shifted = np.roll(ccf_norm, i, axis=1)
-        acf[:, i] = np.sum(ccf_norm * ccf_shifted, axis=1) / acf0
+        acf.append(np.sum(ccf_norm * ccf_shifted, axis=1) / acf0)
 
     # re-normalize the ACF
-    acf /= acf.mean(axis=1, keepdims=True)
+    acf = np.array(acf) / np.mean(acf, axis=0)
 
-    return acf
+    return acf.T
